9fb8cac78301a472b107a53d8689944e484960b6
Aman <a2004jadaun@gmail.com>
2026-05-07 12:14:04 +0530

    update new railway url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://kisanmitra-production-ff3d.up.railway.app/api'
+  baseURL: 'https://kisanmitra-production-e55b.up.railway.app/api'
 });
 
 api.interceptors.request.use((config) => {
