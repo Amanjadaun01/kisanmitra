103156a18e7627343ec9388f8648595413f0fcb1
Aman <a2004jadaun@gmail.com>
2026-05-07 12:54:05 +0530

    render url final

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://kisanmitra-production-e55b.up.railway.app/api'
+  baseURL:'https://kisanmitra-ug40.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
